2927ffc078722ebfc852ebdd17f7a75c8609b5af
Dalton Nguyen <dalton.nguyen1122@gmail.com>
2026-04-14 21:54:41 +1000

    Add VNet and Subnet

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,3 +19,19 @@ resource "azurerm_resource_group" "main" {
     tags = var.tags 
 }
 
+resource "azurerm_virtual_network" "main" {
+    name = "vnet-${var.project_name}-${var.environment}"
+    location = azurerm_resource_group.main.location
+    resource_group_name = azurerm_resource_group.main.name
+    address_space = ["10.0.0.0/16"]
+    tags = var.tags
+
+}
+
+resource "azurerm_subnet" "workload" {
+    name    = "snet-workload-${var.environment}"
+    resource_group_name = azurerm_resource_group.main.name
+    virtual_network_name = azurerm_virtual_network.main.name
+    address_prefixes = ["10.0.1.0/24"]
+  
+}
